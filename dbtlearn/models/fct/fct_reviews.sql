0036e6d37bac275cd4abff7d43f87042de39ba9a
Luciano Filho <lvgalvaofilho@gmail.com>
2023-02-18 14:37:54 -0300

    add macros

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -9,7 +9,9 @@ WITH src_reviews AS (
     SELECT * FROM {{ ref('bronze_reviews')}}
 )
 
-SELECT * FROM src_reviews
+SELECT 
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'review_text']) }} as review_id,
+* FROM src_reviews
 WHERE review_text is not null
 
 {% if is_incremental() %}
